29bd39dd75dc98b7e8e5110e7ff479634757e4be
Justin-Ross-43210 <47845621+Justin-Ross-43210@users.noreply.github.com>
2019-03-12 15:13:15 +0200

    Updated GETMethod.js
    This update includes all the main body of the module. Will be updated once the functions from other modules have been declared

--- GETMethod.js
+++ GETMethod.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
+//var authenticate = require('./Authenticate.js')
+//var authenticate = require('./Register.js')
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.post('/', function(req, res)
+{
+	switch(req.body.type)
+	{
+		case "authenticate":
+			//will pass req.body.image
+			//run authenticate function
+			//return clientid or error
+			res.send("1");
+		break;
+		case "register":
+			//will pass req.body.clientID
+			//will pass req.body.images (images is a JSON array)
+			//run register function
+			res.send("2");
+		break;
+	}
+	res.send(req.query.var);
+});
+
+app.listen(3000);
