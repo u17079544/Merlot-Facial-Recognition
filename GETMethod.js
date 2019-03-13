f9e2d44bb3d84fd01df222ddb928ce45934ed5ee
Justin-Ross-43210 <47845621+Justin-Ross-43210@users.noreply.github.com>
2019-03-13 16:48:57 +0200

    Update GETMethod.js
    Added Log section, renamed Register section to Update and added fake response Data.

--- GETMethod.js
+++ GETMethod.js
@@ -2,31 +2,59 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-var authenticate = require('./Authenticate.js')
-//var Register = require('./Register.js')
+//var authenticate = require('./Authenticate.js')
+//var authenticate = require('./Register.js')
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.post('/', function(req, res)
 {
-	switch(req.body.type)
+	if(req.body.hasOwnProperty("type"))
 	{
-		case "authenticate":
-			//will pass req.body.image
-			//run authenticate function
-			//return clientid or error
-			var clientID = authenticate.Authentication(req.body.image);
-			res.send(clientID);
-		break;
-		case "register":
-			//will pass req.body.clientID
-			//will pass req.body.images (images is a JSON array)
-			//run register function
-			res.send("2");
-		break;
+		switch(req.body.type)
+		{
+			case "authenticate":
+				//will pass req.body.image
+				//run authenticate function
+				//return clientid or error
+				var obj = {clientid: 1234};
+				res.send(JSON.stringify(obj));
+			break;
+			case "update":
+				//will pass req.body.clientid
+				//will pass req.body.images (images is a JSON array)
+				//run register function
+				var obj = {success: true};
+				res.send(JSON.stringify(obj));
+			break;
+			case "log":
+				//will pass req.body.start
+				//will pass req.body.end
+				//run log function
+				//return array of date and clientid as json object
+				var now = new Date();
+				var jsonDate = now.toJSON();
+				var obj = 	{
+								log: [
+									{
+										date: jsonDate,
+										clientid: 1234
+									},
+									{
+										date: jsonDate,
+										clientid: 2345
+									}
+								]
+							};
+				res.send(JSON.stringify(obj));
+			break;
+		}
 	}
-	res.send(req.query.var);
+	else
+	{
+		res.send("incorrect format");
+	}
 });
 
 app.listen(3000);
