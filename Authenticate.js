54cf37199d7bf1f9950e3488bfd40e4e38da4805
CoolKayTwo <47747480+CoolKayTwo@users.noreply.github.com>
2019-03-13 13:46:42 +0200

    Update Authenticate.js

--- Authenticate.js
+++ Authenticate.js
@@ -16,17 +16,22 @@ Authentication.prototype.images = null;
 /** @function toBase64 */
 /**
  * Convert an image into a Base64 string.
- * @param {string} base64 - The Base64 string of the image.
+ * @param {Object} img - The image in jpg/gif/png.
+ * @returns {String} - The image converted to Base64
  */
 Authentication.prototype.toBase64 = function(img) {
+  let imageBuffer = new Buffer(img);
+  return imageBuffer.toString("base64");
 }
 
 /** @function toImage */
 /**
  * Convert a Base64 string into an image.
  * @param {string} base64 - The Base64 string of the image.
+ * @returns {Object} - The image in jpg/gif/png.
  */
 Authentication.prototype.toImage = function(base64) {
+  return Buffer.from(base64,"base64");
 }
 
 /** @function train */
@@ -34,6 +39,7 @@ Authentication.prototype.toImage = function(base64) {
  * Train the model with images and a classification label.
  * @param {Array} imgsClient - The images of the client.
  * @param {string} lblClient - The client name or ID.
+ * @returns {JSONObject} -  The trained model in json.
  */
 Authentication.prototype.train = function(imgsClient,lblClient) {
   if (imgsClient != null && lblClient != "") {
@@ -46,34 +52,27 @@ Authentication.prototype.train = function(imgsClient,lblClient) {
 /** @function authenticate */
 /**
  * Recognize the face of the client.
- * @param {object} img - The image of the client.
+ * @param {Object} img - The image of the client.
+ * @returns {JSONObject} - Information of client or Error has occurred.
  */
 Authentication.prototype.authenticate = function(img) {
+  const fr = require("face-recognition");
+  const recognizer = fr.FaceRecognizer();
+  const db = require("database.js");
+  const table = db.getDataBank();
+  var clientID = "";
+  
+  table.forEach(function(row) {
+    var modelState = row.model;
+    recognizer.load(modelState);
+    var prediction = recognizer.predict(img);
+  });
+  
+  if (clientID == "")
+    return error;
+  return clientID;
 }
 
-//load model from db
-Authentication.prototype.loadModel = function() {
-}
-
-//load images from db
-Authentication.prototype.loadImages = function() {
-}
-
-//load image from db
-Authentication.prototype.loadImage = function() {
-}
-
-//save model to db
-Authentication.prototype.saveModel = function() {
-}
-
-//save images to db
-Authentication.prototype.saveImages = function() {
-}
-
-//save image to db
-Authentication.prototype.saveImage = function() {
-}
 
 //Exporting module
 modules.exports = new Authentication();
