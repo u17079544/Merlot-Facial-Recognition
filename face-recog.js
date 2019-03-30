62d20dee8923309ac44cd4fef8e10b82341d4902
CoolKayTwo <47747480+CoolKayTwo@users.noreply.github.com>
2019-03-30 14:25:15 +0200

    Update face-recog.js

--- face-recog.js
+++ face-recog.js
@@ -1,60 +1,60 @@
-const db = require(''); //future database
-const e = require('exceptions.js');
+const db = require('database.js');
+//const e = require('exceptions.js');
 const ip = require('image-processor.js');
 const fr = require('face-recognition');
 
 const recognizer = fr.FaceRecognizer();
 
-const train_model = (json_base64_images, user_id) => {
+const train_model = (client_id, json_base64_images) => {
 	var face_training_set = ip.process_images(json_base64_images);
 	var face_model = null; 
 	
 	//const number_of_jitter = 1;
-	//recognizer.addFaces(face_training_set, user_id, number_of_jitter);
+	//recognizer.addFaces(face_training_set, client_id, number_of_jitter);
 	
-	recognizer.addFaces(face_training_set, user_id);
+	recognizer.addFaces(face_training_set, client_id);
 	face_model = recognizer.serialize();
 
 	return face_model;
 };
-
+/* Might not be needed.
 const save_model = (trained_model) => {
 	//save all model in database
 };
-
+*/
 const load_models = () => {
 	//get all models from database.
 };
 
-const authenticate_user = (user_base64_image) => {
+const authenticate_user = (client_base64_image) => {
 	const required_accuracy = 0.85;
 	var prediction_accuracy = 0;
-	var user_id = '';
+	var client_id = '';
 	var face_prediction = null;
 	var face_models = load_models();
-	var user_face = ip.process_image(user_base64_image);
+	var client_face = ip.process_image(client_base64_image);
 	
 
 	for (var i=0; i < face_models.length; i++) {
 		recognizer.load(face_models[i]);
-		face_prediction = recognizer.predict(user_face);
+		face_prediction = recognizer.predict(client_face);
 
-		user_id = face_prediction[0].className;
+		client_id = face_prediction[0].className;
 		prediction_accuracy = 1 - face_prediction[0].distance;
 
 		if (prediction_accuracy >= required_accuracy)
-			return user_id;
+			return client_id;
 	}
 
 	//log that authenticaion failed if such info is logged
 
-	return 'No match found.';
+	throw 'No match found.';
 };
 
 module.exports = {
 	authenticate_user,
-	train_model,
-	save_model
+	train_model//,
+	//save_model
 }
 
 
