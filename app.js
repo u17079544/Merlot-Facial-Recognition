84572d488c6c88b99f4e712e816b5975f6b3041f
u17079544 <u17079544@tuks.co.za>
2019-04-02 10:57:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 const app = express()
 
 exports.express = express
