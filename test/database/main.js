399e6b131668f0b6dc59b12f77f1bd6e1b0a7239
Emma Coetzer <u17074526@tuks.co.za>
2019-04-02 13:50:24 +0200

    Updated main.js
    Added testing of the Delete function.

--- test/database/main.js
+++ test/database/main.js
@@ -3,7 +3,15 @@ var http = require("http");
 
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
+
+	//Testing inserting a new client
 	var json = {"1" : "sueifhoadoiada", "2" : "iqohhfascnzcjzdfd", "3" : "oefheodahcjsdnfushdf", "4" : "ioadhfdsnjvnjjsrjf", "5" : "iaofsdjcnxkvsrshg"};
-	dataset.Insert("1234", json);
+	var client_id = dataset.Insert("1234", json);
+	console.log(client_id);
+
+	//Testing deleting a client
+	client_id = dataset.Delete("1234");
+	console.log(client_id);
+
 	res.end();
 }).listen(8000);
