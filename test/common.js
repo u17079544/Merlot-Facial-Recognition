1a4be75c648af7afd6f5b0d7d2ec70c2d4a4aef8
Herbert <u17079544@tuks.co.za>
2019-04-04 01:29:54 +0200

    Update common.js

--- test/common.js
+++ test/common.js
@@ -1,2 +1,9 @@
-var request = require('request');
-var expect = require('chai').expect;
+var chai = require("chai");
+
+var options = {
+    foo: "foo"
+};
+
+exports.options = options;
+exports.chai = chai;
+exports.assert = chai.assert;
