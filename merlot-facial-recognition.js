72c012ce58e9b56ef725117fe7898ab0d2212649
Herbert <u17079544@tuks.co.za>
2019-04-04 01:30:58 +0200

    Update merlot-facial-recognition.js

--- merlot-facial-recognition.js
+++ merlot-facial-recognition.js
@@ -13,7 +13,7 @@ const database = require('./backend/database/database.js')
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.listen(port, "0.0.0.0", () => console.log(`listening on ${ port }`))
+app.listen(port, () => console.log(`listening on ${ port }`))
 
 // Handle requests.
 // API requests
@@ -22,4 +22,4 @@ app.post('/', api.authHandler);
 app.get('/', (req, res) => res.render('pages/index')) // index page
 
 // Database
-http.createServer(database.run).listen(5001);
+// http.createServer(database.run).listen(5001);
